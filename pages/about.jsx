0c52bbeac888fe407364a3fb702086a72a43f153
uziel78 <rune.husa78@gmail.com>
2022-09-21 23:27:59 +0200

    module aliases added to files

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,8 @@
-import Layout from "../components/Layout";
+import Layout from '@/components/Layout';
 
 export default function AboutPage() {
   return (
-    <Layout title="About DJ Events">
+    <Layout title='About DJ Events'>
       <h1>AboutPage</h1>
       <p>This is an app to find the latest DJ and other musical events</p>
       <p>Version 1.0.0</p>

55c4c0873efcf568434d19dfa3f8408e62981871
uziel78 <rune.husa78@gmail.com>
2022-09-22 23:05:47 +0200

    single event with serversideprops

--- config/index.jsx
+++ config/index.jsx
@@ -1,4 +1,6 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_API_URL ||
+  'http://localhost:3000' ||
+  'http://localhost:3001';
 
 //NEXT_PUBLIC is added in environment, before API_URL, to make it visible to client.

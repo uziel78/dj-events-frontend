34041a9aaf50943773609855128dc86a74338601
uziel78 <rune.husa78@gmail.com>
2022-09-22 21:18:32 +0200

    getServerSideProps in index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,23 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+  // logs client-side/browser
+  //console.log(events);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  //console.log(events);  -logs server-side/terminal
+
+  return {
+    props: { events },
+  };
 }

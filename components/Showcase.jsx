69a70959e92d62006255bff9aa12f1522347c3dd
uziel78 <rune.husa78@gmail.com>
2022-09-22 20:54:45 +0200

    tmp API routes and json content

--- components/Showcase.jsx
+++ components/Showcase.jsx
@@ -3,7 +3,7 @@ import styles from '@/styles/Showcase.module.css';
 export default function Showcase() {
   return (
     <div className={styles.showcase}>
-      <h1>Welcome to the PArty</h1>
+      <h1>Welcome to the Party</h1>
       <h2>Find the hottest DJ events</h2>
     </div>
   );
